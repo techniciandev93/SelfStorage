a2e9f800da53c3d1455af221d39df99e053828b2
Weffy61 <weffy@mail.ru>
2023-11-18 07:58:48 +0300

    Add field price to admin

--- renta_warehouse/admin.py
+++ renta_warehouse/admin.py
@@ -11,7 +11,7 @@ class OrderAdmin(admin.ModelAdmin):
 
 @admin.register(Box)
 class BoxAdmin(admin.ModelAdmin):
-    fields = ('number', 'warehouse', 'floor', 'length', 'width', 'height', 'square')
+    fields = ('number', 'warehouse', 'floor', 'length', 'width', 'height', 'price', 'square')
     readonly_fields = ('square',)
 
 
