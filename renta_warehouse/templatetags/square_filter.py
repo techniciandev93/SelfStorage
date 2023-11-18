82535d5f20c00f496727539431d0ebf5c7c37811
Weffy61 <weffy@mail.ru>
2023-11-18 15:47:16 +0300

    Add js buttons for box < 3, box > 10

--- renta_warehouse/templatetags/square_filter.py
+++ renta_warehouse/templatetags/square_filter.py
@@ -3,6 +3,11 @@ from django import template
 register = template.Library()
 
 
+@register.filter(name='square_less_3')
+def square_filter(boxes):
+    return [box for box in boxes if box['square'] < 3]
+
+
 @register.filter(name='square_less_10')
 def square_filter(boxes):
     return [box for box in boxes if box['square'] < 10]
@@ -10,4 +15,4 @@ def square_filter(boxes):
 
 @register.filter(name='square_more_10')
 def square_filter(boxes):
-    return [box for box in boxes if box['square'] < 10]
+    return [box for box in boxes if box['square'] > 10]
