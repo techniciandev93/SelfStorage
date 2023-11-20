c9c63cec67e8c01337f9eaf1072b5e7fda474ae2
anton <a.solovjev@vzv.su>
2023-11-20 14:58:31 +0300

    update registration

--- selfstorage/settings.py
+++ selfstorage/settings.py
@@ -9,12 +9,13 @@ from environs import Env
 env = Env()
 env.read_env()
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 YOOKASSA_SECRET_KEY = env.str('YOOKASSA_SECRET_KEY')
 YOOKASSA_SHOP_ID = env.int('YOOKASSA_SHOP_ID')
